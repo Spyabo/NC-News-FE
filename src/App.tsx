8c188544ca9e026dfbe5c6f2c7f5963ee2ac1164
Spyabo <reesewright02@protonmail.com>
2023-04-17 22:19:48 +0100

    Routes and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Nav-bar/Header";
+import Articles from "./pages/Articles";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Articles />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
