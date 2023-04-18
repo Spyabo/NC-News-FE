3d7c84e2717506b48c39a052f40d476713cf2c87
Spyabo <reesewright02@protonmail.com>
2023-04-18 16:25:53 +0100

    Add link to the list item

--- src/components/ArticleListItem.tsx
+++ src/components/ArticleListItem.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Article } from "../api";
 
 export default function ArticleListItem({ article }: { article: Article }) {
@@ -12,21 +13,23 @@ export default function ArticleListItem({ article }: { article: Article }) {
   };
 
   return (
-    <div
-      style={{
-        position: "relative",
-        border: "2px solid white",
-        padding: "10px",
-      }}
-    >
-      <img
-        src={article.article_img_url}
-        alt="Background Image"
-        style={backgroundStyle as any}
-      />
-      <h3>{article.title}</h3>
-      <p>{`Topic: ${article.topic}`}</p>
-      <p>{`Author: ${article.author}`}</p>
-    </div>
+    <Link to={`/articles/${article.article_id}`}>
+      <div
+        style={{
+          position: "relative",
+          border: "2px solid white",
+          padding: "10px",
+        }}
+      >
+        <img
+          src={article.article_img_url}
+          alt="Background Image"
+          style={backgroundStyle as any}
+        />
+        <h3>{article.title}</h3>
+        <p>{`Topic: ${article.topic}`}</p>
+        <p>{`Author: ${article.author}`}</p>
+      </div>
+    </Link>
   );
 }
